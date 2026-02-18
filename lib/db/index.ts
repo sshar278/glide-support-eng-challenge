da7889a0ee0eab003e57ae2b81e2f82c5ac4003b
sshar278 <sshar278@asu.edu>
2026-02-18 15:34:01 -0800

    Fix DB path inconsistency between app and utility scripts

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,7 +2,9 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema";
 
-const dbPath = "bank.db";
+import path from "path";
+const dbPath = path.join(process.cwd(), "bank.db");
+
 
 const sqlite = new Database(dbPath);
 export const db = drizzle(sqlite, { schema });
